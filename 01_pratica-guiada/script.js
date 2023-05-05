d9973499b4bf82c689eaf4d29a1e91eef7d71c6b
FeZanette <fernandazanette1975@gmail.com>
2023-05-05 15:31:45 -0300

    exercicios aula operacoes

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,74 @@
-//CODIGO AQUI
+// PRÁTICA 1 - ARITMÉRICA
+
+let saldo = 15
+
+saldo = saldo + 22.40
+// saldo +=22.40
+// console.log (saldo)
+
+saldo = saldo - 12.34
+// console.log(saldo);
+
+saldo = saldo /2
+// saldo /=2
+// console.log(saldo);
+
+saldo = saldo * 3.40
+// saldo *= 3.40
+// console.log(saldo);
+
+saldo = saldo % 3
+// saldo %= 3
+console.log (saldo)
+
+
+// ------------------------
+// PRÁTICA 1 - PRECEDÊNCIA MATEMÁTICA
+
+let operacao1 = 2*2+2
+console.log(operacao1);
+
+let operacao2 = 2*(2+2)
+console.log(operacao2);
+
+// -----------------------
+// PRÁTICA 2- IGUAL E DIFERENTE
+
+const comparacao1 = 5===5
+console.log(comparacao1); //true
+
+const comparacao2 = 5!==5
+console.log(comparacao2); //false
+
+const comparacao3 = 5!=="5"
+console.log(comparacao3); //true
+
+const comparacao4= "5"==="cinco"
+console.log(comparacao4); //true
+
+const comparacao5 = typeof 5 === typeof 20
+console.log(comparacao5); //true
+
+const comparacao6 = typeof "5" === typeof "cinco"
+console.log(comparacao6); //true
+ 
+
+// ------------------------
+// PRÁTICA 3 - MENOR E MAIOR (OU IGUAL)
+
+const primeiroNumero = 57
+const segundoNumero = 13
+
+const verificacao1 = primeiroNumero === segundoNumero
+console.log(verificacao1);
+
+const verificacao2 = primeiroNumero <= segundoNumero
+console.log(verificacao2);
+
+const verificacao3 = primeiroNumero > segundoNumero
+console.log(verificacao3);
+
+const verificacao4 = primeiroNumero <= segundoNumero
+console.log(verificacao4);
+
+
